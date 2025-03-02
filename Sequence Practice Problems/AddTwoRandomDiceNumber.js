b1572e55499830f2222490b93e4f2872642837e3
abhi5926 <abhisheksen5926@gmail.com>
2025-03-02 13:08:50 +0530

    [Abhishek] Add. Add Two random Dice Number Problem Solution

---
+++ Sequence Practice Problems/AddTwoRandomDiceNumber.js
@@ -0,0 +1,4 @@
+let firstDiceNumber = Math.floor(Math.random()*6)+1;
+let secondDiceNumber = Math.floor(Math.random()*6)+1;
+let sum = firstDiceNumber + secondDiceNumber;
+console.log("The Addition of Two Dice Number is :"+sum);
